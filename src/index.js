7e2d07f7dbf3ddaab4d429e8c7403acf2eb2f3f5
mufasilmuf <84844774+mufasilmuf@users.noreply.github.com>
2021-09-11 12:23:23 +0530

    Header component added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-ReactDOM.render(<div></div>, document.getElementById("root"));
+import Header from "./common/header/Header";
+ReactDOM.render(
+  <div>
+    <Header></Header>
+  </div>,
+  document.getElementById("root")
+);

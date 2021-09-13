ee5c2681e2dafb90c1133bb8f49ba83825eec64b
mufasilmuf <84844774+mufasilmuf@users.noreply.github.com>
2021-09-13 14:41:53 +0530

    Filter component added

--- src/common/releasedMovielist/RealisedMovielist.js
+++ src/common/releasedMovielist/RealisedMovielist.js
@@ -3,24 +3,78 @@ import "./ReleasedMovielist.css";
 import ImageList from "@material-ui/core/ImageList";
 import ImageListItem from "@material-ui/core/ImageListItem";
 import ImageListItemBar from "@material-ui/core/ImageListItemBar";
+import CardFilter from "../movieFilter/MovieFilter";
+import genres from "../genre";
+import artists from "../artists";
 import { Link } from "react-router-dom";
 class ReleasedMovielist extends Component {
+  state = { Movieslist: this.props.movieList };
+
+  findMovie = "";
+
+  onTextchange = (e) => {
+    debugger;
+    this.findMovie = e.target.value;
+  };
+
+  handeleClick = () => {
+    console.log("handle clicked");
+    debugger;
+    if (this.findMovie !== "") {
+      let filterMovieList = this.state.Movieslist.filter((movie) => {
+        return (
+          movie.title.toLowerCase().indexOf(this.findMovie.toLowerCase()) >= 0
+        );
+      });
+      this.setState({ Movieslist: filterMovieList });
+    } else {
+      this.setState({ Movieslist: this.props.movieList });
+    }
+  };
+
+  handeleClickDetails = (e) => {
+    debugger;
+    var movieDetails = e.target.alt;
+    console.log(movieDetails);
+    localStorage.setItem("MovieDetails", movieDetails);
+  };
+
   render() {
     return (
       <div className="flex-container">
         <div className="left">
-          <Link to="/Details">
-            <ImageList rowHeight={350} cols={4}>
-              {this.props.movieList.map((movie) => {
-                return (
-                  <ImageListItem key={movie.id}>
-                    <img src={movie.poster_url} alt={movie.title} />
-                    <ImageListItemBar title={movie.title} />
-                  </ImageListItem>
-                );
-              })}
-            </ImageList>
-          </Link>
+          <ImageList rowHeight={380} cols={5}>
+            {this.state.Movieslist.map((movie) => {
+              return (
+                <Link to="/Details">
+                  <div
+                    className="MovieTabs"
+                    key={movie.id}
+                    id={movie.id}
+                    onClick={this.handeleClickDetails}
+                  >
+                    <ImageListItem key={movie.id}>
+                      <img
+                        src={movie.poster_url}
+                        alt={movie.title}
+                        className="MovieTabsImages"
+                      />
+                      <ImageListItemBar key={movie.title} title={movie.title} />
+                    </ImageListItem>
+                  </div>
+                </Link>
+              );
+            })}
+          </ImageList>
+        </div>
+
+        <div className="right">
+          <CardFilter
+            OnTextChange={this.onTextchange}
+            HandeleClick={this.handeleClick}
+            MovieGenres={genres}
+            MovieArtists={artists}
+          />
         </div>
       </div>
     );
--- src/common/upcomingMovieList.js/upcomingmovelist.js
+++ src/common/upcomingMovieList.js/upcomingmovelist.js
@@ -6,6 +6,13 @@ import "./upcomingmovelist.css";
 import { Link } from "react-router-dom";
 
 class Imagelist extends Component {
+  handeleClickDetails = (e) => {
+    debugger;
+    var movieDetails = e.target.alt;
+    console.log(movieDetails);
+    localStorage.setItem("MovieDetails", movieDetails);
+  };
+
   render() {
     debugger;
     return (
@@ -13,7 +20,7 @@ class Imagelist extends Component {
         <ImageList className="mainContainer" rowHeight={250} cols={6}>
           {this.props.movieList.map((movie) => {
             return (
-              <ImageListItem key={movie.id} className="container">
+              <ImageListItem key={movie.id} onClick={this.handeleClickDetails}>
                 <img src={movie.poster_url} alt={movie.title} />
                 <ImageListItemBar title={movie.title}></ImageListItemBar>
               </ImageListItem>

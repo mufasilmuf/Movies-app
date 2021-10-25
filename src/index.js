e6b3afe38ff89d7c1582e765de19e983395d1d13
mufasilmuf <84844774+mufasilmuf@users.noreply.github.com>
2021-10-25 13:32:21 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './common/common.css';
+import 'typeface-roboto';
+import registerServiceWorker from './registerServiceWorker';
+import Controller from './screens/Controller';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Controller />, document.getElementById('root'));
+registerServiceWorker();
